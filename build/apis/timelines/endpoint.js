ba0230be22812e9913deaf5d30cf4ca8023068af
songpengyi <psong@encoredtech.com>
2018-02-21 19:19:01 -0800

    run build

--- build/apis/timelines/endpoint.js
+++ build/apis/timelines/endpoint.js
@@ -1,10 +1,16 @@
 "use strict";
 
 module.exports = {
-  timelines: function timelines(siteId) {
+  timelines: function timelines(registrationId) {
+    return "/1.2/devices/" + registrationId + "/timelines";
+  },
+  timeline: function timeline(registrationId, timelineId) {
+    return "/1.2/devices/" + registrationId + "/timelines/" + timelineId;
+  },
+  timelinesBySite: function timelinesBySite(siteId) {
     return "/1.2/sites/" + siteId + "/timelines";
   },
-  timeline: function timeline(siteId, timelineId) {
+  timelineBySite: function timelineBySite(siteId, timelineId) {
     return "/1.2/sites/" + siteId + "/timelines/" + timelineId;
   }
 };
--- build/apis/timelines/index.js
+++ build/apis/timelines/index.js
@@ -3,10 +3,16 @@
 var endpoint = require('./endpoint');
 
 var timelines = {
-  timelines: function timelines(siteId, params) {
+  timelines: function timelines(registrationId, params) {
+    return this.get(endpoint.timelines(registrationId), { params: params });
+  },
+  updateTimeline: function updateTimeline(registrationId, timelineId, data) {
+    return this.put(endpoint.timeline(registrationId, timelineId), data || {});
+  },
+  timelinesBySite: function timelinesBySite(siteId, params) {
     return this.get(endpoint.timelines(siteId), { params: params });
   },
-  updateTimeline: function updateTimeline(siteId, timelineId, data) {
+  updateTimelineBySite: function updateTimelineBySite(siteId, timelineId, data) {
     return this.put(endpoint.timeline(siteId, timelineId), data || {});
   }
 };
